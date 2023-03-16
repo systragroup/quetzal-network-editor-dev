a61aa15a7e85972665ea85296a8d87de7065d909
simon boivin <sboivin@systra.com>
2023-03-16 09:39:19 -0400

    e96df0f

--- docs/config.js
+++ docs/config.js
@@ -1,2 +1,2 @@
-"use strict";(self.webpackChunkquetzal_network_editor=self.webpackChunkquetzal_network_editor||[]).push([[182],{9182:(e,t,a)=>{a.d(t,{C:()=>n});const n="pk.eyJ1Ijoic2JvaXZpbiIsImEiOiJja3BlMm41cm4xa2QyMnZwZTBqdGx4bHI0In0.11oH2-B2g9J6LarDnoqQfQ"}}]);
+"use strict";(self.webpackChunkquetzal_network_editor=self.webpackChunkquetzal_network_editor||[]).push([[182],{79182:(e,t,a)=>{a.d(t,{C:()=>n});const n="pk.eyJ1Ijoic2JvaXZpbiIsImEiOiJja3BlMm41cm4xa2QyMnZwZTBqdGx4bHI0In0.11oH2-B2g9J6LarDnoqQfQ"}}]);
 //# sourceMappingURL=config.js.map
