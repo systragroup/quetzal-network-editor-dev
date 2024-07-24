4ae192a747f80a79f0a21515261d864517cc524e
Simon Boivin <sboivin@systra.com>
2024-07-24 15:33:54 -0400

    fix read params json in zip with trim

--- docs/assets/StaticLayer-BSoOELwO.js
+++ docs/assets/StaticLayer-BSoOELwO.js
@@ -1,4 +1,4 @@
-import{c1 as Lr,c as zt,c2 as Ho,u as Ze,r as dr,l as zr,e as Qr,ac as Wr,c3 as Uo,_ as Rt,j as ce,k as ve,m as Hr,L as Zo,M as Jo,S as Lt,y as se,K as Qo,i as Ko,ab as Vo,h as rf,f as ef,Y as af,o as tf,ag as nf,q as uf,z as He}from"./index-aXarI9U5.js";var Pt={exports:{}};/**
+import{c1 as Lr,c as zt,c2 as Ho,u as Ze,r as dr,l as zr,e as Qr,ac as Wr,c3 as Uo,_ as Rt,j as ce,k as ve,m as Hr,L as Zo,M as Jo,S as Lt,y as se,K as Qo,i as Ko,ab as Vo,h as rf,f as ef,Y as af,o as tf,ag as nf,q as uf,z as He}from"./index-D1LJfkDP.js";var Pt={exports:{}};/**
  * chroma.js - JavaScript library for color conversions
  *
  * Copyright (c) 2011-2019, Gregor Aisch
