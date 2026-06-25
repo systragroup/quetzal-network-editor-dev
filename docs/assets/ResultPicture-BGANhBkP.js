fac58d65edaea73119b7d745586d25e3737ff201
Simon Boivin <sboivin@systra.com>
2026-06-25 14:44:57 -0400

    deploy with script

---
+++ docs/assets/ResultPicture-BGANhBkP.js
@@ -0,0 +1,1 @@
+import{am as s,a as t,an as a,e as n,g as c,j as e}from"./index-DhVT_mjt.js";import{M as i}from"./MarkDownViewer-DtLVRwrD.js";import"./Markdown-DbULogg2.js";const d=s({__name:"ResultPicture",setup(l){const r=t(),o=a();return(m,p)=>(n(),c(i,{model:e(o).model,scenario:e(o).scenario+"/","file-paths":e(r).otherFiles},null,8,["model","scenario","file-paths"]))}});export{d as default};
